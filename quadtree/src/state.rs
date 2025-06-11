f08aeaa60a6e2213ee3a9f371af2d89a36e1422b
adambigg-s <biggsa601@gmail.com>
2025-06-11 16:59:07 -0400

    barnes hut

--- quadtree/src/state.rs
+++ quadtree/src/state.rs
@@ -5,13 +5,11 @@ use sokol::app as sapp;
 use crate::barnes_hut::BarnesHutWrapper;
 use crate::quadtree::PositionPlanar;
 use crate::quadtree::QuadTree;
-use crate::utils;
 use crate::utils::mouse_to_screen;
 use crate::utils::positive_rand_range_vec2;
 use crate::utils::wait;
 use crate::utils::zero_centered_range_vec2;
 use crate::utils::BoundingBox;
-use crate::utils::EPSILON;
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -71,17 +69,17 @@ impl State {
             dimensions: BoundingBox::build(Vec2::ZERO, Vec2::new(width as f32, height as f32)),
             particles: Vec::new(),
             config: SimulationConfig {
-                starting_spawn: 100,
-                gravity: 1e3,
-                epsilon_squared: 50.,
-                theta: 0.5,
+                starting_spawn: 10000,
+                gravity: 1e2,
+                epsilon_squared: 10.,
+                theta: 2_f32.sqrt() / 2.,
                 velocity_rand_max: 50.,
                 mass_rand_max: 100.,
                 frame_time_dt_mod: 0.1,
                 neighbor_distance: 300.,
             },
             quadtree: QuadTree::build(
-                1,
+                3,
                 BoundingBox::build(Vec2::ZERO, Vec2::new(width as f32, height as f32)),
             ),
         }
@@ -93,6 +91,26 @@ impl State {
         });
         // spawns one big particle in the middle
         self.particles.push(Particle::new(self.dimensions.max / 2., Vec2::ZERO, 10000.));
+    }
+
+    pub fn handle_event(&mut self, event: sapp::Event) {
+        if event.mouse_button == sapp::Mousebutton::Left && event._type == sapp::EventType::MouseDown {
+            self.particles.push(Particle::new(
+                mouse_to_screen(event.mouse_x, event.mouse_y, &self.dimensions),
+                Vec2::ZERO,
+                self.config.mass_rand_max,
+            ));
+            // small delay to prevent like 100 particles spawning and stack overflow
+            wait(5);
+        }
+        if event.key_code == sapp::Keycode::R {
+            self.particles.clear();
+        }
+    }
+
+    pub fn update_dimensions(&mut self, width: f32, height: f32) {
+        self.dimensions.max = Vec2::new(width, height);
+        self.quadtree.root().boundary = self.dimensions;
     }
 
     #[allow(dead_code)]
@@ -137,35 +155,15 @@ impl State {
         let mut barnes_hut = BarnesHutWrapper::new();
         barnes_hut.build_hierarchy(&self.quadtree, &self.particles);
 
-        for target_index in 0..self.particles.len() {
+        (0..self.particles.len()).for_each(|target_index| {
             let acceleration = self.barnes_hut_force_recursive(&barnes_hut, target_index);
             self.particles[target_index].acceleration = acceleration;
-        }
+        });
 
         self.particles.iter_mut().for_each(|particle| {
             particle.update(dt);
             particle.constrain(&self.dimensions);
         });
-    }
-
-    pub fn handle_event(&mut self, event: sapp::Event) {
-        if event.mouse_button == sapp::Mousebutton::Left && event._type == sapp::EventType::MouseDown {
-            self.particles.push(Particle::new(
-                mouse_to_screen(event.mouse_x, event.mouse_y, &self.dimensions),
-                Vec2::ZERO,
-                self.config.mass_rand_max,
-            ));
-            // small delay to prevent like 100 particles spawning and stack overflow
-            wait(5);
-        }
-        if event.key_code == sapp::Keycode::R {
-            self.particles.clear();
-        }
-    }
-
-    pub fn update_dimensions(&mut self, width: f32, height: f32) {
-        self.dimensions.max = Vec2::new(width, height);
-        self.quadtree.root().boundary = self.dimensions;
     }
 
     fn add_random_particle(&mut self) {
@@ -185,11 +183,10 @@ impl State {
     }
 
     fn barnes_hut_force_recursive(&mut self, barnes_hut: &BarnesHutWrapper, target_index: usize) -> Vec2 {
-        let target_particle = &self.particles[target_index];
         let mut acceleration = Vec2::ZERO;
         self.calculate_force_recursive(
             QuadTree::ROOT_INDEX,
-            target_particle,
+            target_index,
             &self.quadtree,
             barnes_hut,
             &mut acceleration,
@@ -199,32 +196,29 @@ impl State {
     }
 
     fn calculate_force_recursive(
-        &self, node_index: usize, target_particle: &Particle, tree: &QuadTree, barnes_hut: &BarnesHutWrapper,
+        &self, node_index: usize, target_index: usize, tree: &QuadTree, barnes_hut: &BarnesHutWrapper,
         acceleration: &mut Vec2,
     ) {
         let Some(node_data) = barnes_hut.barnes_hut_data[node_index]
         else {
             return;
         };
+        let target_particle = self.particles[target_index];
 
         let pointing = node_data.mass_center - target_particle.position;
-        let distance = pointing.length();
-
-        if distance < EPSILON {
+        let sq_radius = pointing.length_squared();
+        if sq_radius < self.config.epsilon_squared {
             return;
         }
 
-        let s_over_d = tree.nodes[node_index].boundary.max_dimension() / distance;
-
+        let s_over_d = tree.nodes[node_index].boundary.max_dimension() / sq_radius.sqrt();
         if s_over_d < self.config.theta || tree.nodes[node_index].leaves.is_none() {
-            let sq_radius = distance * distance + self.config.epsilon_squared;
-
             let force_magnitude = self.config.gravity * target_particle.mass * node_data.mass / sq_radius;
             *acceleration += pointing.normalize() * force_magnitude / target_particle.mass;
         }
         else if let Some(leaf_start) = tree.nodes[node_index].leaves {
             (leaf_start..(leaf_start + QuadTree::STEM_LEAF_COUNT)).for_each(|leaf_index| {
-                self.calculate_force_recursive(leaf_index, target_particle, tree, barnes_hut, acceleration);
+                self.calculate_force_recursive(leaf_index, target_index, tree, barnes_hut, acceleration);
             });
         }
     }
